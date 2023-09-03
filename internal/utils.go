f78e3cb9ff76101e4d0d55f59ddeeb010b511047
Amaan Khan <amaanmkhan2@gmail.com>
2023-09-03 01:20:34 -0500

    Add getMachineNames

--- internal/utils.go
+++ internal/utils.go
@@ -12,3 +12,20 @@ func exitOnError(err error) {
 	fmt.Fprintln(os.Stderr, message)
 	os.Exit(1)
 }
+
+// Returns a tuple of the current machine's hostname and an array of all the machine names
+func getMachineNames(machineNameFormat string, numMachines int) (string, []string) {
+	thisMachineName, err := os.Hostname()
+	if err != nil
+		utils.exitOnError(err)
+
+	machineNames = make([]string, 0)
+	
+	for i := 1; i <= numMachines; i++ {
+		newName = fmt.Sprintf(machineNameFormat, i)
+		if newName != thisMachineName
+			machineNames = append(machineNames, newName)
+	}
+
+	return thisMachineName, machineNames
+}
